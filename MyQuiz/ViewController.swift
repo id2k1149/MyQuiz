f033d208185faa70ed55edf786035599fac07a20
id2k1149 <id2k1149@gmail.com>
2022-10-30 19:26:49 -0400

    Initial Commit

---
+++ MyQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyQuiz
+//
+//  Created by Max Franz Immelmann on 10/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
