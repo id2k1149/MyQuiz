765d4ce9314fdf718b04237ab4038310fd65a5d5
id2k1149 <id2k1149@gmail.com>
2022-10-31 11:42:32 -0400

    add question array with some questions

--- MyQuiz/Models/Question.swift
+++ MyQuiz/Models/Question.swift
@@ -16,3 +16,42 @@ enum ResponseType {
     case multiple
     case ranged
 }
+
+extension Question {
+    
+    static func getQuestions() -> [Question] {
+        // questions array
+          [
+            Question(
+                title: "What food do you prefer?",
+                type: .single,
+                answers: [
+                    Answer(title: "Steak", type: .dog),
+                    Answer(title: "Fish", type: .cat),
+                    Answer(title: "Carrot", type: .rabbit),
+                    Answer(title: "Corn", type: .frog)
+                ]
+            ),
+            Question(
+                title: "What do you like more?",
+                type: .multiple,
+                answers: [
+                    Answer(title: "Swim", type: .dog),
+                    Answer(title: "Sleep", type: .cat),
+                    Answer(title: "Hug", type: .rabbit),
+                    Answer(title: "Eat", type: .frog)
+                ]
+            ),
+            Question(
+                title: "Do you like car trips?",
+                type: .ranged,
+                answers: [
+                    Answer(title: "Hate", type: .cat),
+                    Answer(title: "I'm nervous", type: .rabbit),
+                    Answer(title: "Don't notice", type: .frog),
+                    Answer(title: "Love", type: .dog)
+                ]
+            ),
+        ]
+    }
+}
