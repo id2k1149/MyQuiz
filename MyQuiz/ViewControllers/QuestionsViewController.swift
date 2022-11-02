a7c599b7613c8c06cd7c360b400b41d1618b1ec5
id2k1149 <id2k1149@gmail.com>
2022-11-02 11:11:57 -0400

    add outlets and actions to QuestionsViewController

---
+++ MyQuiz/ViewControllers/QuestionsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  MyQuiz
+//
+//  Created by Max Franz Immelmann on 10/30/22.
+//
+
+import UIKit
+
+class QuestionsViewController: UIViewController {
+    
+    @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet var rangedStackView: UIStackView!
+    @IBOutlet var rangedSlider: UISlider!
+    @IBOutlet var rangesLabels: [UILabel]!
+    
+    private let questions = Question.getQuestions()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func singleAnswerButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func multipleAnswerButtonTapped() {
+    }
+    @IBAction func rangedAnswerButtonTapped() {
+    }
+    
+    // memory check
+    deinit {
+        print("QuestionsViewController has been deallocated")
+    }
+
+
+}
+
